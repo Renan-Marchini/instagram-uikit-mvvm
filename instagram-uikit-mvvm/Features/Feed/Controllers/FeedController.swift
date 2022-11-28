75b7f1b2b13ed2a485739b18f8a8e1c66d8834a4
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-11-28 19:36:04 -0300

    🐣 Create FeedController scaffolding

---
+++ instagram-uikit-mvvm/Features/Feed/Controllers/FeedController.swift
@@ -0,0 +1,32 @@
+//
+//  FeedController.swift
+//  instagram-uikit-mvvm
+//
+//  Created by Renan Andrusiac on 28/11/22.
+//
+
+import UIKit
+
+// MARK: - FeedController
+
+class FeedController: UIViewController {
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
+
+// MARK: - View Code
+
+extension FeedController: ViewCode {
+    func buildHierarchy() {
+        
+    }
+    
+    func setupConstrains() {
+        
+    }
+}
