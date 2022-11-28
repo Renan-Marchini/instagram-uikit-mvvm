a87d868443c484c13c372b72e9d386f27affcb3c
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-11-28 17:58:14 -0300

    first commit - create project

---
+++ instagram-uikit-mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram-uikit-mvvm
+//
+//  Created by Renan Andrusiac on 28/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
