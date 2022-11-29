96956dbb08c510c44b2c5e732d1c0f902c471cc9
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-11-29 18:00:34 -0300

    implement UICollectionViewDataSource funcs

--- instagram-uikit-mvvm/Features/Feed/Controllers/FeedController.swift
+++ instagram-uikit-mvvm/Features/Feed/Controllers/FeedController.swift
@@ -23,6 +23,20 @@ class FeedController: UICollectionViewController {
     }
 }
 
+// MARK: - UICollectionViewDataSource
+
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .systemBlue
+        return cell
+    }
+}
+
 // MARK: - View Code
 
 extension FeedController: ViewCode {
