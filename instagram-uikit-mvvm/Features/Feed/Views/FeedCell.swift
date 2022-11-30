00ccfa7c9e471396ef816a0aa78b89758cc4ca2e
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-11-30 09:14:43 -0300

    ✨ Implement FeedCell profile image

--- instagram-uikit-mvvm/Features/Feed/Views/FeedCell.swift
+++ instagram-uikit-mvvm/Features/Feed/Views/FeedCell.swift
@@ -5,11 +5,30 @@
 //  Created by Renan Andrusiac on 29/11/22.
 //
 
+import SnapKit
 import UIKit
 
 // MARK: - FeedCell
 
 class FeedCell: UICollectionViewCell {
+
+    // MARK: - Dimensions
+
+    private enum Dimension {
+        static let profileImageSize = 40.0
+        static let profileImageOffiset = 12
+    }
+
+    // MARK: - Properties
+
+    private lazy var profileImage: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = UIImage(named: "iron-profile")
+        return iv
+    }()
 
     // MARK: - Life Time
 
@@ -27,14 +46,24 @@ class FeedCell: UICollectionViewCell {
 
 extension FeedCell: ViewCode {
     func buildHierarchy() {
-        
+        addSubview(profileImage)
     }
 
     func configViews() {
         backgroundColor = .systemCyan
+
+        profileImage.layer.cornerRadius = Dimension.profileImageSize / 2
     }
     
     func setupConstrains() {
-        
+        profileImage.snp.makeConstraints { make in
+            make.top
+                .left
+                .equalToSuperview()
+                .offset(Dimension.profileImageOffiset)
+            make.height
+                .width
+                .equalTo(Dimension.profileImageSize)
+        }
     }
 }
