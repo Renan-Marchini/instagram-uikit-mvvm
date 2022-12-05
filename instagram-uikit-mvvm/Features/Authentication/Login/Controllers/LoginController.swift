c251912c0c9730a1650f0cd342b0987c4526f4dc
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-12-05 15:19:38 -0300

    ✨feat: add mainStackView in LoginController

--- instagram-uikit-mvvm/Features/Authentication/Login/Controllers/LoginController.swift
+++ instagram-uikit-mvvm/Features/Authentication/Login/Controllers/LoginController.swift
@@ -11,12 +11,21 @@ import UIKit
 
 class LoginController: UIViewController {
 
-    // MARK: - Color
+    // MARK: - Enums
 
     private enum Color {
         static let firstColorBackground = UIColor.systemPurple.cgColor
         static let secondColorBackground = UIColor.systemBlue.cgColor
     }
+
+    private enum Dimension {
+        static let stackViewSpacing = 20.0
+        static let stackViewOffice = 32.0
+    }
+
+    // MARK: - Properties
+
+    private lazy var mainStackView = buildStackView()
 
     // MARK: - Life Cycle
 
@@ -36,10 +45,32 @@ extension LoginController: ViewCode {
         )
     }
     func buildHierarchy() {
-        
+        view.addSubview(mainStackView)
     }
     
     func setupConstrains() {
-        
+        mainStackView.snp.makeConstraints { make in
+            make.top
+                .equalTo(view.safeAreaLayoutGuide.snp.top)
+                .offset(Dimension.stackViewOffice)
+            make.left
+                .equalToSuperview()
+                .offset(Dimension.stackViewOffice)
+            make.right
+                .equalToSuperview()
+                .offset(-Dimension.stackViewOffice)
+            
+        }
+    }
+}
+
+// MARK: - Builders
+
+extension LoginController {
+    private func buildStackView() -> UIStackView {
+        let sk = UIStackView()
+        sk.axis = .vertical
+        sk.spacing = Dimension.stackViewSpacing
+        return sk
     }
 }
