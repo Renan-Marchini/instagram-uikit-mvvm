613702ddc6eb7e81a1cafde3182ea253e26428c1
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-12-05 14:49:21 -0300

    🐣 create LoginController scaffolding

---
+++ instagram-uikit-mvvm/Features/Authentication/Login/Controllers/LoginController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginController.swift
+//  instagram-uikit-mvvm
+//
+//  Created by Renan Andrusiac on 05/12/22.
+//
+
+import UIKit
+
+// MARK: - LoginController
+
+class LoginController: UIViewController {
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+}
+
+// MARK: - View Code
+
+extension LoginController: ViewCode {
+    func configViews() {
+        
+    }
+    func buildHierarchy() {
+        
+    }
+    
+    func setupConstrains() {
+        
+    }
+}
