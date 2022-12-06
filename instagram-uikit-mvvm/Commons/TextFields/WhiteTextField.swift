4d44e0990f2431154d3c06c7e3c2d4b2ef311c99
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-12-06 08:48:28 -0300

    change placeholderFontColor alpha from 0.87 to 0.7

--- instagram-uikit-mvvm/Commons/TextFields/WhiteTextField.swift
+++ instagram-uikit-mvvm/Commons/TextFields/WhiteTextField.swift
@@ -14,10 +14,17 @@ class WhiteTextField: UITextField {
     // MARK: - Attributes
 
     private enum Color {
-        static let backgroundColor = UIColor(white: 1, alpha: 0.1)
-        static let placeholderFontColor = UIColor(white: 1.0, alpha: 0.87)
+        static let backgroundColor = UIColor(
+            white: 1,
+            alpha: 0.1
+        )
+        static let placeholderFontColor = UIColor(
+            white: 1.0,
+            alpha: 0.7
+        )
         static let textFontColor = UIColor.white
     }
+
     private enum Dimension {
         static let height = 50.0
         static let leftSpace = 20.0
