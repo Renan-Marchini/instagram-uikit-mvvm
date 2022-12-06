0b3f6225e4cea88bfbead554725379af1ff38d8a
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-12-06 18:48:07 -0300

    🐣 SignUpController

---
+++ instagram-uikit-mvvm/Features/Authentication/SignUp/Controllers/SignUpController.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpController.swift
+//  instagram-uikit-mvvm
+//
+//  Created by Renan Andrusiac on 06/12/22.
+//
+
+import UIKit
+
+// MARK: - SignUpController
+
+class SignUpController: UIViewController {
+
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+}
+
+// MARK: - View Code
+
+extension SignUpController: ViewCode {
+    func buildHierarchy() {
+        
+    }
+    
+    func configViews() {
+    }
+    
+    func setupConstrains() {
+        
+    }
+}
