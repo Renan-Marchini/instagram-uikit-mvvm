95035f910a6ce7813ee51f207098568731494ed9
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-12-06 18:58:20 -0300

    SignUpController - add mainStackView

--- instagram-uikit-mvvm/Features/Authentication/SignUp/Controllers/SignUpController.swift
+++ instagram-uikit-mvvm/Features/Authentication/SignUp/Controllers/SignUpController.swift
@@ -17,6 +17,17 @@ class SignUpController: UIViewController {
         static let firstColorBackground = UIColor.systemPurple.cgColor
         static let secondColorBackground = UIColor.systemBlue.cgColor
     }
+    
+    private enum Dimension {
+        static let pushImageButtonSize = 80.0
+        static let pushImageButtonSpacing = 32.0
+        static let stackViewSpacing = 20.0
+        static let stackViewOffice = 32.0
+    }
+
+    // MARK: - Properties
+
+    private lazy var mainStackView = buildStackView()
 
     // MARK: - Life Cycle
 
@@ -30,7 +41,7 @@ class SignUpController: UIViewController {
 
 extension SignUpController: ViewCode {
     func buildHierarchy() {
-        
+        view.addSubview(mainStackView)
     }
     
     func configViews() {
@@ -38,9 +49,31 @@ extension SignUpController: ViewCode {
             firstColor: Color.firstColorBackground,
             secondColor: Color.secondColorBackground
         )
+        mainStackView.backgroundColor = .systemOrange
     }
     
     func setupConstrains() {
-        
+        mainStackView.snp.makeConstraints { make in
+            make.top
+                .equalTo(view.safeAreaLayoutGuide.snp.top)
+                .offset(Dimension.stackViewOffice)
+            make.left
+                .equalToSuperview()
+                .offset(Dimension.stackViewOffice)
+            make.right
+                .equalToSuperview()
+                .offset(-Dimension.stackViewOffice)
+        }
+    }
+}
+
+// MARK: - Builders
+
+extension SignUpController {
+    private func buildStackView() -> UIStackView {
+        let sk = UIStackView()
+        sk.axis = .vertical
+        sk.spacing = Dimension.stackViewSpacing
+        return sk
     }
 }
