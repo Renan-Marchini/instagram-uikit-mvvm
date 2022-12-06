2ea934f30250f8b9cb14cc7353462f105e03d22c
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-12-06 18:06:02 -0300

    🔀 change: rename PurpleButton variables generically

--- instagram-uikit-mvvm/Commons/Buttons/PurpleButton.swift
+++ instagram-uikit-mvvm/Commons/Buttons/PurpleButton.swift
@@ -14,14 +14,14 @@ class PurpleButton: UIButton {
     // MARK: - Attributes
 
     private enum Color {
-        static let passwordButtonBackground = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-        static let passwordButtonFont = UIColor.white
+        static let buttonBackground = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        static let buttonFont = UIColor.white
     }
 
     private enum Dimension {
-        static let passwordButtonHeight = 50.0
-        static let passwordButtonCornerRadius = 5.0
-        static let passwordButtonFont = 20.0
+        static let buttonHeight = 50.0
+        static let buttonCornerRadius = 5.0
+        static let buttonFont = 20.0
     }
 
     // MARK: - Life Time
@@ -43,16 +43,16 @@ extension PurpleButton: ViewCode {
     func buildHierarchy() {}
     
     func configViews() {
-        backgroundColor = Color.passwordButtonBackground
-        layer.cornerRadius = Dimension.passwordButtonCornerRadius
-        setTitleColor(Color.passwordButtonFont, for: .normal)
+        backgroundColor = Color.buttonBackground
+        layer.cornerRadius = Dimension.buttonCornerRadius
+        setTitleColor(Color.buttonFont, for: .normal)
         titleLabel?.font = UIFont
-            .boldSystemFont(ofSize: Dimension.passwordButtonFont)
+            .boldSystemFont(ofSize: Dimension.buttonFont)
     }
     
     func setupConstrains() {
         self.snp.makeConstraints { make in
-            make.height.equalTo(Dimension.passwordButtonHeight)
+            make.height.equalTo(Dimension.buttonHeight)
         }
     }
 }
